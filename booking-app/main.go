9987934b01b5117b338259ed8df04b4c3fff3b4b
Noor-30412 <2100030412@kluniversity.in>
2023-12-25 17:48:53 +0530

    Merged changes

--- booking-app/main.go
+++ booking-app/main.go
@@ -1,23 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	vehicalName := "Bus"      // value can be changed for var
 	const vehicalTickets = 50 // value cannot be changed for const
 	var remainingTickets uint = 50
-	// Array initialization
-	// var bookings [50]string
 
-	// Slice
 	var bookings []string
 
-	// fmt.Printf("Welcome To My %T Booking Application\n",vehicalName)
 	fmt.Printf("Welcome To My %v Booking Application\n", vehicalName)
 	fmt.Println("We have a total of ", vehicalTickets, "tickets and", remainingTickets, "are still available.")
 	fmt.Println("   Get Your Tickets Here    ")
 
-	for remainingTickets>0 {
+	for remainingTickets > 0 {
 		var userName string
 		var userBooked uint
 		var userMail string
@@ -28,9 +27,20 @@ func main() {
 		fmt.Println("Enter No of Tickets:")
 		fmt.Scan(&userBooked)
 
-		if userBooked <= remainingTickets {
+		if userBooked > 50 {
+			fmt.Printf("We only have %v tickets remaining\n", remainingTickets)
+			continue
+		}
+		fmt.Println("Enter Your Email:")
+		fmt.Scan(&userMail)
+		fmt.Println("Enter Your Mobile Number:")
+		fmt.Scan(&userMobile)
+
+		var isValidName = len(userName) >= 2
+		isValidEmail := strings.Contains(userMail, "@")
+
+		if isValidEmail && isValidName && userBooked <= remainingTickets {
 			remainingTickets = remainingTickets - userBooked
-			// bookings[0] = userName
 			bookings = append(bookings, userName)
 
 			fmt.Printf("Thank you %v you Booked %v Tickets. You will receive confirmation @ %v and Mobile %v\n", userName, userBooked, userMail, userMobile)
@@ -39,19 +49,12 @@ func main() {
 			fmt.Printf("The People booked Tickets are %v\n", bookings)
 			fmt.Printf("The No Of People booked Tickets are %v\n", len(bookings))
 
-			// var noTickets bool = remainingTickets == 0
-			// noTickets := remainingTickets == 0
 			if remainingTickets == 0 {
 				fmt.Println("The Bus is Full")
 				break
 			}
-		} else {
-			fmt.Printf("We only have %v tickets remaining\n", remainingTickets)
-			continue
+		} else if !isValidEmail || !isValidName {
+			fmt.Println("Invalid Input")
 		}
-		fmt.Println("Enter Your Email:")
-		fmt.Scan(&userMail)
-		fmt.Println("Enter Your Mobile Number:")
-		fmt.Scan(&userMobile)
 	}
 }
